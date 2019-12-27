63a89d3e6237a1c9a582f045b5355a3b15b6020f
Matthew Raynard <matthewraynard65@gmail.com>
2019-12-26 18:03:19 -0800

    stop sign

--- rpi/camera/signdetection.py
+++ rpi/camera/signdetection.py
@@ -1,5 +1,13 @@
 import cv2
+import time
+import imutils
 import numpy as np
 
 def process(image):
-	
+
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    stop_sign = cv2.CascadeClassifier.detectMultiScale(gray,1.02,10)
+
+    cv2.rectangle(img,(x,y),(x+2,y+h),(255,0,0),2)
